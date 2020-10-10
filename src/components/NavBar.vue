9fd67464a761e008b35e517ba904f3efba46cdb6
pupsi4ek <sda2000den@gmail.com>
2020-10-10 13:34:11 +0300

    added db.json + routing

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link to="/">
+          Home
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/profile">
+          Profile
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/login">
+          Login
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/">
+          Logout
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home";
+import Login from "@/views/Login";
+import Cabinet from '@/views/Cabinet'
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/profile',
+    name: 'Cabinet',
+    component: Cabinet
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
 ];
 
